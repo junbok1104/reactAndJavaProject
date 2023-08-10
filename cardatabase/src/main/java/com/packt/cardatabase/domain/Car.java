84c8781468167740509c4f74b645262665e7a10f
junbok <bock1104@gmail.com>
2023-08-10 21:22:45 +0900

    Revert "CrudRepositroy, PagingAndSortingRepositry사용 및 1:N,N:N관계 표현"
    This reverts commit f95bb6bce689cb389c7cd76da902fd034d08d839.

--- cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -2,24 +2,16 @@ package com.packt.cardatabase.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
-//1대다 관계용
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "owner")
-	private Owner owner;
 	
 	private String brand, model, color, registerNumber;
 	
@@ -30,23 +22,14 @@ public class Car {
 	
 	public Car() {	}
 
-	public Car(Owner owner, String brand, String model, String color, String registerNumber, int year, int price) {
+	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
 		super();
-		this.owner = owner;
 		this.brand = brand;
 		this.model = model;
 		this.color = color;
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
-	}
-
-	public Owner getOwner() {
-		return owner;
-	}
-
-	public void setOwner(Owner owner) {
-		this.owner = owner;
 	}
 
 	public long getId() {
--- cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository2.java
+++
@@ -1,8 +0,0 @@
-package com.packt.cardatabase.domain;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CarRepository2 extends PagingAndSortingRepository<Car, Long>{
-
-	//페이징 매김과 정렬을 적용하기 위해 사용할 때 PagingAndSortingRepository 를 사용
-}
