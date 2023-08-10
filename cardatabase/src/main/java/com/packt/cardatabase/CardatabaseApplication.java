4bddc54ec0d66e721c76e749ea9537606420b6f7
junbok <bock1104@gmail.com>
2023-08-10 21:31:09 +0900

    CrudRepository,PagingAndSortingRepository 사용 및 1:N, n:m관계 표현

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,18 +1,52 @@
 package com.packt.cardatabase;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner{
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	
+	@Autowired
+	private CarRepository carRepository;
+	
+	@Autowired
+	private OwnerRepository ownerRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 		logger.info("Application Started");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//소유자 객체를 추가하고 DB에 저장
+		Owner owner1 = new Owner("John", "Johnson");
+		Owner owner2 = new Owner("Mary", "Robinson");
+		ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+		
+		//CommandLineRunner를 이용하여 H2 DB에 데이터를 먼저 넣기 위해 사용
+		carRepository.save(new Car(owner1, "Ford", "Mustang", "Red", "ADF-1121", 2021, 59000));
+		carRepository.save(new Car(owner2, "Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000));
+		carRepository.save(new Car(owner2, "Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000));
+		
+		for(Car car : carRepository.findAll()) { 
+			logger.info(car.getBrand() + " " + car.getModel());
+		}
+		
 	}
 
 }
---
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,67 @@
+package com.packt.cardatabase.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Owner {
+//1대다 관계용
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long ownerid;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	private List<Car> cars;
+	
+	private String firstname, lastname;
+	
+	public Owner() {}
+
+	public Owner(String firstname, String lastname) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+
+	public Long getOwnerid() {
+		return ownerid;
+	}
+
+	public void setOwnerid(Long ownerid) {
+		this.ownerid = ownerid;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	};
+	
+	
+}
