12fe9ac782911ad36d07e8abea0bea0a9c7a7f3d
junbok <bock1104@gmail.com>
2023-09-19 22:24:24 +0900

    Spring Security 이용한 권한 체크 기능 추가

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -13,6 +13,8 @@ import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.Owner;
 import com.packt.cardatabase.domain.OwnerRepository;
+import com.packt.cardatabase.domain.User;
+import com.packt.cardatabase.domain.UserRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner{
@@ -24,6 +26,9 @@ public class CardatabaseApplication implements CommandLineRunner{
 	
 	@Autowired
 	private OwnerRepository ownerRepository;
+	
+	@Autowired
+	private UserRepository urRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -38,14 +43,22 @@ public class CardatabaseApplication implements CommandLineRunner{
 		Owner owner2 = new Owner("Mary", "Robinson");
 		ownerRepository.saveAll(Arrays.asList(owner1, owner2));
 		
-		//CommandLineRunner를 이용하여 H2 DB에 데이터를 먼저 넣기 위해 사용
-		carRepository.save(new Car(owner1, "Ford", "Mustang", "Red", "ADF-1121", 2021, 59000));
-		carRepository.save(new Car(owner2, "Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000));
-		carRepository.save(new Car(owner2, "Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000));
+		// 자동차 객체를 추가하고 소유자 객체에 연결하며 데이터베이스에 저장
+		Car car1 = new Car(owner1,"Ford", "Mustang", "Red","ADF-1121",2021,59000);
+		Car car2 = new Car(owner2,"Nissan", "Mustang", "White","SSJ-3002",2019,29000);
+		Car car3 = new Car(owner2,"Toyota", "Prius", "Silver","KKO-0212",2020,39000);
 		
+		carRepository.saveAll(Arrays.asList(car1, car2, car3));
+			
 		for(Car car : carRepository.findAll()) { 
 			logger.info(car.getBrand() + " " + car.getModel());
 		}
+		
+		// 사용자 이름 : user, 암호 : user
+		urRepository.save(new User("user", "$2a$04$JuVoz7PT10hDD65Rw5dRCu4MH1gweZHmO1NLgaZf5ecum4CMx4BJ6", "USER"));
+
+		// 사용자 이름 : admin, 암호 : admin
+		urRepository.save(new User("admin", "$2a$04$bSrQJM9j46.PKKP9Wlvr/eoDak9sgGyoyB9E/26mJlpJpTD4rawIO", "ADMIN"));
 		
 	}
 
