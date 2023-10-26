68f15e9875872e3f72f080ee4fd1642a32c50879
junbok <bock1104@gmail.com>
2023-10-26 21:22:52 +0900

    JWT를 이용한 백엔드 보호

--- cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -49,20 +49,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		
 		// 모든 사용자가 모든 엔드포인트에 접근할 수 있게 한다.
-		http.csrf().disable().cors().and()
-			.authorizeRequests().anyRequest().permitAll();
-		
 //		http.csrf().disable().cors().and()
-//			.sessionManagement()
-//			.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//			.authorizeRequests()
-//			// /login 엔드포인트에 대한 POST 요청은 보호되지 않음.
-//			.antMatchers(HttpMethod.POST, "/login").permitAll()
-//			// 다른 모든 요청은 보호됨
-//			.anyRequest().authenticated().and()
-//			.exceptionHandling()
-//			.authenticationEntryPoint(exceptionHandler).and()
-//			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//			.authorizeRequests().anyRequest().permitAll();
+		
+		// 인가된 사용자만 사용할 수 있도록 백엔드 보호
+		http.csrf().disable().cors().and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.authorizeRequests()
+			// /login 엔드포인트에 대한 POST 요청은 보호되지 않음.
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			// 다른 모든 요청은 보호됨
+			.anyRequest().authenticated().and()
+			.exceptionHandling()
+			.authenticationEntryPoint(exceptionHandler).and()
+			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	// 클래스에 전역 CORS 필터 추가
